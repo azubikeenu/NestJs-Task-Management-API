f1b350a20abf8d514e2e078e9d41488ad64fc039
Enu Azubike <enuazubike88@gmail.com>
2023-03-30 00:19:45 +0100

    check for empty fields on stack status validation

--- src/tasks/pipes/tast-status-validation.pipe.ts
+++ src/tasks/pipes/tast-status-validation.pipe.ts
@@ -4,13 +4,16 @@ import { TaskStatus } from '../task-status.enum';
 export class TaskStatusValidationPipe implements PipeTransform {
   private readonly ALLOWED_STATUSES: string[] = Object.values(TaskStatus);
   transform(value: any) {
-    value = value.toUpperCase();
-    if (!this.isValidStatus(value))
-      throw new BadRequestException(
-        `Invalid status ${value}!,must be one of ${this.ALLOWED_STATUSES.join(
-          ',',
-        )}`,
-      );
+    value = value?.toUpperCase();
+    if (!this.isValidStatus(value)) {
+      const message = value
+        ? `Invalid status ${value}!,must be one of ${this.ALLOWED_STATUSES.join(
+            ' ',
+          )}`
+        : `status is required`;
+      throw new BadRequestException(message);
+    }
+
     return value;
   }
 
