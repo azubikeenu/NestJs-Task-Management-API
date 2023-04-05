08148db8d14e5777bb038119a6bbd3c94e379e47
Enu Azubike <enuazubike88@gmail.com>
2023-04-05 01:43:18 +0100

    handled exceptions for  validating passwordd

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,6 +2,7 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  Logger,
 } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -9,6 +10,7 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
+  private logger = new Logger('user_repository');
   constructor(dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
   }
@@ -23,17 +25,25 @@ export class UserRepository extends Repository<User> {
       if ((err.code = '23505')) {
         throw new ConflictException('username already exists');
       } else {
-        throw new InternalServerErrorException();
+        this.logger.error(err?.stack);
+        throw new InternalServerErrorException(
+          'failed to persist user to the database',
+        );
       }
     }
   }
 
   async validatePassword({ username, password }: AuthCredentialsDto) {
-    const user = await this.findOneBy({ username });
-    if (user && (await user.comparePassword(password, user.password))) {
-      return user.username;
-    } else {
-      return null;
+    try {
+      const user = await this.findOneBy({ username });
+      if (user && (await user.comparePassword(password, user.password))) {
+        return user.username;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      this.logger.error(error?.stack);
+      throw new InternalServerErrorException(`failed to validate credentials`);
     }
   }
 }
