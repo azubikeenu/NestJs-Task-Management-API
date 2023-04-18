54d25ecc7ededb1a11388747e0189b4f15e9de6e
Enu Azubike <enuazubike88@gmail.com>
2023-04-18 08:22:44 +0100

    finished tests for taskService

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -21,6 +21,8 @@ const mockTaskRepository = () => ({
   getAllTasks: jest.fn(),
   findOneBy: jest.fn(),
   createTask: jest.fn(),
+  delete: jest.fn(),
+  save: jest.fn(),
 });
 
 describe('Task Service', () => {
@@ -37,21 +39,25 @@ describe('Task Service', () => {
         },
       ],
     }).compile();
+
     taskService = module.get<TasksService>(TasksService);
     taskRepository = module.get<TaskRepository>(TaskRepository);
   });
 
   describe(`getTasks`, () => {
     it(`Should get all tasks from the repository`, async () => {
+      // because getAllTasks() returns a promise we use mockResolvedValue() instead of mockReturnedValue()
       taskRepository.getAllTasks.mockResolvedValue('tasks');
 
       expect(taskRepository.getAllTasks).not.toHaveBeenCalled();
+
       const filterDto: GetFilteredTaskDto = {
         searchTerm: 'foo',
         status: TaskStatus.IN_PROGRESS,
       };
 
       const tasks = await taskService.getTasks(filterDto, mockUser);
+
       expect(taskRepository.getAllTasks).toHaveBeenCalled();
       expect(tasks).toBe('tasks');
     });
@@ -75,6 +81,7 @@ describe('Task Service', () => {
 
     it(`Should throw an error when task with id is not found`, () => {
       taskRepository.findOneBy.mockResolvedValue(null);
+
       expect(taskService.getTaskById(1, mockUser)).rejects.toThrow(
         NotFoundException,
       );
@@ -95,6 +102,47 @@ describe('Task Service', () => {
       const result = await taskService.createTask(createTaskDto, mockUser);
       expect(result.title).toBe(createTaskDto.title);
       expect(result.description).toBe(createTaskDto.description);
+    });
+  });
+
+  describe(`deleteTask`, () => {
+    it(`Should delete the task from the repository`, () => {
+      taskRepository.delete.mockResolvedValue({ affected: 1 });
+      expect(() => taskService.deleteTask(1, mockUser)).not.toThrow();
+    });
+
+    it(`Should throw an exception when task is not found in the database`, () => {
+      taskRepository.delete.mockResolvedValue({ affected: 0 });
+      expect(taskService.deleteTask(1, mockUser)).rejects.toThrow();
+    });
+  });
+
+  describe(`updateStatus`, () => {
+    it(`Should update the status in the db and return updated task`, async () => {
+      const taskStub = {
+        id: 1,
+        title: 'Task',
+        description: 'another task',
+        status: TaskStatus.OPEN,
+      };
+
+      taskRepository.findOneBy.mockResolvedValue(taskStub);
+      const newStatus: TaskStatus = TaskStatus.IN_PROGRESS;
+      taskRepository.save.mockResolvedValue({ ...taskStub, status: newStatus });
+
+      const result = await taskService.updateStatus(1, newStatus, mockUser);
+
+      expect(result).toBeDefined();
+      expect(result.status).toEqual(newStatus);
+    });
+
+    it(`Should throw an exception if the task isn't present in the db`, async () => {
+      const newStatus: TaskStatus = TaskStatus.DONE;
+      taskRepository.findOneBy.mockResolvedValue(null);
+      expect(taskRepository.save).toBeCalledTimes(0);
+      expect(
+        taskService.updateStatus(1, newStatus, mockUser),
+      ).rejects.toThrow();
     });
   });
 });
